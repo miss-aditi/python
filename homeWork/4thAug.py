5a934e3e5823d562cd3d7d8a43c42ecb1a3edc5f
miss-aditi <aditidhaye1@gmail.com>
2026-08-04 21:04:18 +0530

    Add Python learning and homework

---
+++ homeWork/4thAug.py
@@ -0,0 +1,39 @@
+# task 1: create id card
+print(" -- task 1 --")
+print("  ---My ID Card---  ")
+print("Name: Aditi ")
+print("Age: 21")
+print("City: pune")
+print("DOB: 14-3-2005")
+print("Collage: HVPM-COET")
+print("Blood Group: A+")
+print()
+print()
+
+
+# task 2: find data type
+print(" -- task 2 --")
+a = 25
+b = 25.5
+c = "Twenty Five"
+d = True
+e = 2+3j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print()
+print()
+
+# task 3: memory detective
+v1 = 100
+v2 = 200
+v3 = 300
+print("ID of v1 -> ", id(v1))
+print("ID of v2 -> ", id(v2))
+print("ID of v3 -> ", id(v3))
+
+
+
+
